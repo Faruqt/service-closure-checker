0cd5d7ec822191e341605ea9c7cbcf8b8849192c
Faruqt <faruqabdulsalam@yahoo.com>
2024-11-04 01:56:10 +0000

    chore: add documentation and format file

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
-import os
 
+"""
+app.py: This file contains the code to define the CDK app.
+"""
+# library imports
+import os
 import aws_cdk as cdk
 
+# local imports
 from infrastructure.infrastructure_stack import InfrastructureStack
 
 
 app = cdk.App()
-InfrastructureStack(app, "InfrastructureStack",
-
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    )
+InfrastructureStack(
+    app,
+    "InfrastructureStack",
+    env=cdk.Environment(
+        account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")
+    ),
+)
 
 app.synth()
